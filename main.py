6e192ec01d4e5fe9bfdfb318de759cc0a84581ec
Psuedoo <forthewin720@gmail.com>
2021-08-16 09:59:33 -0400

    added endpoint for all inventory properties

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from database.db_handler import DatabaseHandler
 from database.models import Computer
 from utils.util import generate_computer
@@ -14,6 +15,50 @@ for i in range(15):
 async def root():
     return {'computers': handler.search()}
 
+@app.get("/inventory/")
+async def get_inventory(make: str = None):
+    return {'computers': handler.search()}
+
+@app.get("/asset_tag/{asset_tag}")
+async def get_asset_tag(asset_tag: int):
+    return {'computers': handler.search(asset_tag=asset_tag)}
+
+# Need to figure out what's going on here "Latitude 6293"
+@app.get("/model/{model}")
+async def get_model(model: str):
+    model = model.lower().capitalize()
+    return {'computers': handler.search(model=model)}
+
+@app.get("/issued/{is_issued}")
+async def get_issued(is_issued: Optional[bool] = True):
+    return {'computers': handler.search(issued=is_issued)}
+
+# Also look into this. Issue arises when using two words
+@app.get("/issued/issuee/{issuee}")
+async def get_issuee(issuee: str, issued: bool = True):
+    issuee = issuee.lower().capitalize()
+    return {'computers': handler.search(issuee=issuee)}
+
+# Is not working
+@app.get("/on_location/{is_on_location}}")
+async def get_on_location(is_on_location: Optional[bool] = True):
+    return {'computers': handler.search(on_location=is_on_location)}
+
+# Two word issue
+@app.get("/on_location/location/{location}")
+async def get_location(location: str):
+    location = location.lower().capitalize()
+    return {'computers': handler.search(location=location)}
+
+@app.get("/on_location/class_location/{class_location}")
+async def get_class_location(class_location: str):
+    return {'computers': handler.search(class_location=class_location)}
+
+@app.get("/service_tag/{service_tag}")
+async def get_service_tag(service_tag: str):
+    service_tag = service_tag.upper()
+    return {'computers': handler.search(service_tag=service_tag)}
+
 @app.get("/make/{make}")
 async def get_make(make: str):
     make = make.lower().capitalize()
@@ -23,6 +68,11 @@ async def get_make(make: str):
 async def get_checker(checker_name: str):
     checker_name = checker_name.lower().capitalize()
     return {'computers': handler.search(checker=checker_name)}
+
+@app.get("/on_hand/{is_on_hand}")
+async def get_on_hand(is_on_hand: Optional[bool] = True):
+    return {'computers': handler.search(on_hand=is_on_hand)}
+
 
 # print(handler.search(one='one', two='two', make='Dell', service_tag="10"))
 
