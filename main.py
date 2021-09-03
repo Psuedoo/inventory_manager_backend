8a3fed8e9199f1ab889d39ed1cf111802661dfad
Psuedoo <forthewin720@gmail.com>
2021-09-03 14:17:10 -0400

    finally receiving the token

--- main.py
+++ main.py
@@ -24,12 +24,15 @@ generate_data(handler, 50)
 origins = [
     'http://localhost',
     'http://localhost:8000',
-    'http://localhost:8080'
+    'http://localhost:8080',
+    'http://127.0.0.1:5500',
+    'http://127.0.0.1:8000',
+    'http://127.0.0.1:8080',
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
